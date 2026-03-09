491b2e7e61b834f02fab634e97eee63fcca3bba7
jpsteckel <fluidwingz@gmail.com>
2026-03-09 14:43:58 -0400

    decreased tiktok height

--- src/app/components/Tiktok.js
+++ src/app/components/Tiktok.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TikTokEmbed = () => {
 
   return (
-    <div style={{ position: 'relative', height: '100%', overflow: 'hidden' }}>
+    <div style={{ position: 'relative', height: '50%', overflow: 'hidden' }}>
       <blockquote className="tiktok-embed" cite="https://www.tiktok.com/@js1co/video/7598734770461248798" data-video-id="7598734770461248798" style={{maxWidth: '605px', minWidth: '325px'}} > <section> <a target="_blank" title="@js1co" href="https://www.tiktok.com/@js1co?refer=embed">@js1co</a> useful EE IoT project! <a title="electricalengineering" target="_blank" href="https://www.tiktok.com/tag/electricalengineering?refer=embed">#electricalengineering</a> <a title="college" target="_blank" href="https://www.tiktok.com/tag/college?refer=embed">#college</a> <a title="engineering" target="_blank" href="https://www.tiktok.com/tag/engineering?refer=embed">#engineering</a> <a title="stem" target="_blank" href="https://www.tiktok.com/tag/stem?refer=embed">#stem</a> <a title="project" target="_blank" href="https://www.tiktok.com/tag/project?refer=embed">#project</a> <a target="_blank" title="♬ original sound - Luke James" href="https://www.tiktok.com/music/original-sound-7531469368109632278?refer=embed">♬ original sound - Luke James</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>
     </div>
   );
