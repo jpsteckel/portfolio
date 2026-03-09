f4734fd8c32b4f655951eeeb453f667f33dca656
jpsteckel <fluidwingz@gmail.com>
2026-03-09 12:16:43 -0400

    changed text size

--- src/app/page.js
+++ src/app/page.js
@@ -46,8 +46,8 @@ export default function Home() {
             height={400}
             className="relative rounded-sm shadow-lg pointer-events-none"/>
         </div>
-        <div className="relative text-sm font-bold text-black w-2xl text-center pointer-events-none">
-          I created bus departure board for the University of Delaware using an ESP32 and a 128x160 OLED screen. The board displays real-time bus departure information, including the current time, bus route, and departure time. I developed and deployed an AWS Lambda function to scrape data from the university&apos;s ETA Transit website and act as an API for the ESP32, which fetches the data every minute and updates the display. The project was built using python, docker, the Arduino IDE and C++ programming language.
+        <div className="relative text-md font-bold text-black w-2xl text-center pointer-events-none">
+          I created this bus departure board using an ESP32 and a 128x160 OLED screen. The board displays real-time bus departure information, including the current time, bus route, and departure time. I developed and deployed an AWS Lambda function to scrape data from the university&apos;s ETA Transit website and act as an API for the ESP32, which fetches the data every minute and updates the display. The project was built using python, docker, the Arduino IDE and C++ programming language.
         </div>
       </div>
       <div className="relative text-4xl sm:text-4xl font-black text-center text-black pointer-events-none">
