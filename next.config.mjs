e2eb83a116eb41497fdbaf41d8cdd145beb8d952
jpsteckel <fluidwingz@gmail.com>
2026-03-09 10:45:18 -0400

    fixed image bug

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'export', // Enables static exporting
   // Optional: Add a basePath if your site is deployed to a subpath (like GitHub Pages)
-  basePath: '/jsteckel', 
+  //basePath: '/jsteckel', 
   images: {
     unoptimized: true, // Might be necessary for some static hosts if Image Optimization API is not supported
   },
